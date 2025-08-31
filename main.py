4f50cb96eca280c8b9b6526e4a9faac3b7f19d99
mahendrafajar17 <mahendrafajar17@gmail.com>
2025-08-31 21:02:10 +0700

    update

--- main.py
+++ main.py
@@ -11,25 +11,3 @@ def bagi(a, b):
     if b == 0:
         return "Tidak bisa dibagi dengan nol!"
     return a / b
-
-print("Kalkulator Sederhana")
-print("1. Tambah")
-print("2. Kurang")
-print("3. Kali")
-print("4. Bagi")
-
-pilih = input("Pilih operasi (1/2/3/4): ")
-
-angka1 = float(input("Masukkan angka pertama: "))
-angka2 = float(input("Masukkan angka kedua: "))
-
-if pilih == '1':
-    print("Hasil:", tambah(angka1, angka2))
-elif pilih == '2':
-    print("Hasil:", kurang(angka1, angka2))
-elif pilih == '3':
-    print("Hasil:", kali(angka1, angka2))
-elif pilih == '4':
-    print("Hasil:", bagi(angka1, angka2))
-else:
-    print("Pilihan tidak valid.")
